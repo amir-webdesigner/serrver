9882db0c1636646fbb3484e1b00018bc5c8cadfa
amir web designer <108860935+amir-webdesigner@users.noreply.github.com>
2025-07-04 15:25:40 +0330

    Update dukascopy.js

--- api/dukascopy.js
+++ api/dukascopy.js
@@ -16,8 +16,8 @@ res.setHeader('Access-Control-Allow-Origin', '*');
     const data = await getHistoricalRates({
       instrument: instrument.toLowerCase(),
       dates: {
-        from: from,
-        to: to,
+        from: new Date(from),
+        to: new Date(to),
       },
       timeframe: timeframe,
       format: 'json',
